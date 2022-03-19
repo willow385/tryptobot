8319348f89143edb070a978893f84d093b28f7af
Dante Falzone <dantefalzone0@protonmail.com>
2022-03-19 21:24:04 +0000

    started writing parser for *.dnd files

--- dnd-parser.h
+++ dnd-parser.h
@@ -1,3 +1,14 @@
-typedef struct input_reader {
+#include <stdio.h>
 
+typedef struct input_reader {
+  char *text; // text to be parsed
+  size_t pos; // position of current char in the text
+  char (*peek)(struct input_reader *);
+  char (*advance)(struct input_reader *);
+  int  (*has_reached_eof)(struct input_reader *);
 } input_reader_t;
+
+void construct_input_reader(
+  input_reader_t *dest,
+  const char *text
+);
